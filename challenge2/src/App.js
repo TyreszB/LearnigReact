0b72100b43c714feac56ca96bad586f0812efc24
TyreszB <127250161+TyreszB@users.noreply.github.com>
2023-08-10 16:06:59 -0400

    finished state challenge

--- challenge2/src/App.js
+++ challenge2/src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <Counter/>
+    );
 }
 
-export default App;
+function Counter() {
+    const [step, setStep] = useState(1);
+    const [count, setCount] = useState(0);
+    const date = new Date();
+    date.setDate(date.getDate() + count)
+
+    function handlePlus() {
+        setCount((c) => c + 1)
+    }
+
+    function handleMinus() {
+        setCount((c) => c - 1)
+    }
+
+    return (
+        <div>
+            <div>
+                <button onClick={() => setStep((c) => c - 1)}>-</button>
+                <div>Step: {step}</div>
+                <button onClick={() => setStep((c) => c + 1)}>+</button>
+            </div>
+            <div>
+                <button onClick={() => setCount((c) => c - step)}>-</button>
+                <div>Count: {count}</div>
+                <button onClick={() => setCount((c) => c + step)}>+</button>
+            </div>
+            <div>
+                <p>
+                <span>{count === 0 ?
+                    "Today is "
+                    : count > 0
+                        ? `${count} days from today is `
+                        : `${Math.abs(count)} days ago was `}</span>
+                    <span>{date.toDateString()}</span>
+                </p>
+            </div>
+        </div>
+
+
+    )
+}
+
