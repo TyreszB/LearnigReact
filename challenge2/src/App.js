e54ade07a981d234bbb5cfae49ed4b9e32746f7a
TyreszB <127250161+TyreszB@users.noreply.github.com>
2023-08-10 22:58:24 -0400

    added more react components to callange 2

--- challenge2/src/App.js
+++ challenge2/src/App.js
@@ -19,17 +19,20 @@ function Counter() {
     function handleMinus() {
         setCount((c) => c - 1)
     }
+    function handleReset() {
+        setCount(0);
+        setStep(1);
+    }
 
     return (
         <div>
             <div>
-                <button onClick={() => setStep((c) => c - 1)}>-</button>
-                <div>Step: {step}</div>
-                <button onClick={() => setStep((c) => c + 1)}>+</button>
+                <input type="range" min="0" max="10" value={step} onChange={(e) => setStep(Number(e.target.value))} />
+                <span> Step: {step}</span>
             </div>
             <div>
                 <button onClick={() => setCount((c) => c - step)}>-</button>
-                <div>Count: {count}</div>
+                <input type="text" value={count} onChange={e=> setCount(Number(e.target.value))}/>
                 <button onClick={() => setCount((c) => c + step)}>+</button>
             </div>
             <div>
@@ -41,10 +44,12 @@ function Counter() {
                         : `${Math.abs(count)} days ago was `}</span>
                     <span>{date.toDateString()}</span>
                 </p>
+
+                { count !== 0 || step !== 1 ?  <div>
+                    <button onClick={handleReset}>Reset</button>
+                </div> : null }
             </div>
         </div>
-
-
     )
 }
 
